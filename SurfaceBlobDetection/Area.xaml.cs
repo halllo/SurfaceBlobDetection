b46bc9f2cc280c1201f7e3702c7ab5543f1a067a
Manuel Naujoks <mnaujoks@gmail.com>
2016-11-01 22:13:51 +0100

    area

---
+++ SurfaceBlobDetection/Area.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Surface.Presentation.Input;
+
+namespace SurfaceBlobDetection
+{
+	public partial class Area
+	{
+		public Area()
+		{
+			InitializeComponent();
+		}
+
+		private void Canvas_TouchDown(object sender, TouchEventArgs e)
+		{
+			e.TouchDevice.Capture(VisualizerCanvas);
+			e.Handled = true;
+			UpdateLog();
+		}
+
+		private void Canvas_TouchMove(object sender, TouchEventArgs e)
+		{
+			UpdateLog();
+		}
+
+		private void Canvas_TouchUp(object sender, TouchEventArgs e)
+		{
+			if (e.TouchDevice.Captured == VisualizerCanvas)
+			{
+				VisualizerCanvas.ReleaseTouchCapture(e.TouchDevice);
+			}
+			UpdateLog();
+		}
+
+		private void UpdateLog()
+		{
+			Log.Text = "";
+			foreach (TouchDevice device in VisualizerCanvas.GetInputDevicesCaptured())
+			{
+				var data = device.GetEllipseData(VisualizerCanvas);
+				var position = device.GetCenterPosition(this);
+				var tagData = device.GetTagData();
+
+				Log.Text = Log.Text + "\n blob : "
+					+ "Axis=" + data.MajorAxis + "," + data.MinorAxis + "; "
+					+ "Pos=" + position.X + "," + position.Y + "; "
+					+ "Tag=" + tagData.Value + ";";
+			}
+		}
+	}
+}
--- SurfaceBlobDetection/Window1.xaml.cs
+++ SurfaceBlobDetection/Window1.xaml.cs
@@ -29,42 +29,5 @@ namespace SurfaceBlobDetection
 		{
 			e.Cancel = true;
 		}
-
-		private void Canvas_TouchDown(object sender, System.Windows.Input.TouchEventArgs e)
-		{
-			e.TouchDevice.Capture(VisualizerCanvas);
-			e.Handled = true;
-			UpdateLog();
-		}
-
-		private void Canvas_TouchMove(object sender, System.Windows.Input.TouchEventArgs e)
-		{
-			UpdateLog();
-		}
-
-		private void Canvas_TouchUp(object sender, System.Windows.Input.TouchEventArgs e)
-		{
-			if (e.TouchDevice.Captured == VisualizerCanvas)
-			{
-				VisualizerCanvas.ReleaseTouchCapture(e.TouchDevice);
-			}
-			UpdateLog();
-		}
-
-		private void UpdateLog()
-		{
-			Log.Text = "";
-			foreach (TouchDevice device in VisualizerCanvas.GetInputDevicesCaptured())
-			{
-				var data = device.GetEllipseData(VisualizerCanvas);
-				var position = device.GetCenterPosition(this);
-				var tagData = device.GetTagData();
-
-				Log.Text = Log.Text + "\n blob : " 
-					+ "Axis=" + data.MajorAxis + "," + data.MinorAxis + "; " 
-					+ "Pos=" + position.X + "," + position.Y + "; " 
-					+ "Tag=" + (tagData != null ? tagData.Value.ToString() : "<no tag>") + ";";
-			}
-		}
 	}
 }
